c6e87fd893fb1811776bc8a07aa456cbf9b572a8
anveshreddy2006 <padidamanveshreddy.com>
2025-08-20 00:45:54 +0530

    feat: add cloudinary configuration and multer dependency; fix typo in ApiError class

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,8 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+  cloudinary.config({ 
+        cloud_name: 'dviumzu5w', 
+        api_key: '853477415832655', 
+        api_secret: '**********' // Click 'View API Keys' above to copy your API secret
+    });
